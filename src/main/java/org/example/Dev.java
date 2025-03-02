b01d89f907084e1230b462fdafaeae011b7fb2b6
vishvas04 <vishvaskalyan04@gmail.com>
2025-03-02 12:14:57 +0530

    first commit

---
+++ src/main/java/org/example/Dev.java
@@ -0,0 +1,11 @@
+package org.example;
+
+class Dev extends Main{
+    private int age;
+    public void build(){
+        System.out.println("Building with 0 bugs");
+    }
+    void getAge(){
+        System.out.println(age);
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        System.out.println("Hello and welcome!");
+        Dev obj=(Dev) context.getBean("dev");
+        obj.getAge();
+    }
+}
